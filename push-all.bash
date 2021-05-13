cf0db6b7399b5900547dade432da80129c54066d
github@emresult.com <github@emresult.com>
2021-05-13 15:11:23 +0000

    Renamed TAGPREFIX to TAG_PREFIX

--- push-all.bash
+++ push-all.bash
@@ -4,7 +4,7 @@ set -vex
 
 HERE="$( cd "$(dirname "$0")" ; pwd -P )"
 export RELEASE_HASH="${RELEASE_HASH:-$(curl -s https://api.github.com/repos/iterative/dvc/releases/latest | sha256sum | cut -c -8)}"
-export TAGPREFIX="${TAGPREFIX:-dvcorg}"
+export TAG_PREFIX="${TAG_PREFIX:-dvcorg}"
 
 DIR=$(dirname $0)
 cd "${DIR}"
@@ -17,6 +17,11 @@ find $DIR -name Dockerfile | sort | while read -r filepath ; do
     else
         TAG=$(echo "${dockerdir}[3,100]}" | tr '/ ' '--')
     fi
-    echo "PUSHING: ${dockerdir} with the tag: ${TAGPREFIX}/${TAG}"
-    docker push ${TAGPREFIX}/${TAG}
+    if [[ "${TAG}" ~= '.*/.*' ]] ; then
+        echo "PUSHING: ${dockerdir} with the tag: ${TAG}"
+        docker push ${TAG}
+    else 
+        echo "PUSHING: ${dockerdir} with the tag: ${TAG_PREFIX}/${TAG}"
+        docker push ${TAG_PREFIX}/${TAG}
+    fi
 done
